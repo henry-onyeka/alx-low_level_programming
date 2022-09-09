ddff9622e5d08915453614ced65e9a55e85a20f9
hmike54@gmail.com <hmike54@gmail.com>
2022-09-09 12:34:15 -0700

    printing lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stio.h>
+/**
+* main - me just wan do magic here i no send anybody
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	char henry="a";
+	while (henry <= z)
+{
+	putchar("%c ",henry);
+	henry++;
+}
+	putchar("\n");
+	return (0);
+}
