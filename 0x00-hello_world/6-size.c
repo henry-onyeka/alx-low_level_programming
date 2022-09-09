d1c3225fd52aaababc9dc7950d0d8029ef4b4254
hmike54@gmail.com <hmike54@gmail.com>
2022-09-08 19:01:06 -0700

    finally

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ long long long_int;
 //sizeof evaluates the size of a variable
 printf("Size of int: %d byte(s)\n", sizeof(chaType));
 printf("Size of an int: %d byte(s)\n",sizeof(intType));
-printf("Size of a long int: %d byte(s)\n", sizeof(lontype));
+printf("Size of a long int: %d byte(s)\n", sizeof(lontType));
 printf("Size of a long long int: %d bytes(s)\n", sizeof(long_int));
 printf("Size of a float: %d byte(s)\n", sizeof(floatTYpe));
 return (0);
