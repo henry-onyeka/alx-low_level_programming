ae79c4e8adeea47a8c3229bdd8f1603278e45411
hmike54@gmail.com <hmike54@gmail.com>
2022-09-09 10:42:48 -0700

    first if conditional statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
+ * main -Determines if a number is positive, negative or zero
  *
  * Rturn: Always 0 (success)
  */
@@ -12,17 +12,17 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if( n > 0 )
+if(n > 0)
 {
 
 	printf("%d is positive\n", n);
 }
-else if( n < 0 )
+else if(n < 0)
 {
 
 	printf("%d is negative\n", n);
 }
-else if( n == 0 )
+else if(n == 0)
 {
 
 	printf("%d is zero\n", n);
