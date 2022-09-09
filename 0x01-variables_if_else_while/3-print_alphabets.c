e8bfa31b63ac9361761351119d268dc4c95c9f71
hmike54@gmail.com <hmike54@gmail.com>
2022-09-09 15:22:45 -0700

    printing lower and uppercase alphabets in same line

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 /*
- * main - prints the alphabet.betty buhari is your portion thunder fire u
- *
- * Description: print the letters of the alphabet in lower then in uppercase
  *
  * Return: Always 0 (success)
 */
