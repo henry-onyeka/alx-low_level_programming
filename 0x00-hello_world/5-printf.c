fbbe18d84fefde85c60da2bdae1e755fc1285d36
hmike54@gmail.com <hmike54@gmail.com>
2022-09-08 18:16:11 -0700

    with proper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 /*
-* main - Entry point
-* Description:" prints with proper grammar, but the outcome is a piece of
-*
-* art,"
+* main - Entry point*
 * Return: Always 0 (Success)
 */
 int main(void)
