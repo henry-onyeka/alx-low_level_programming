d5b0218bec25ba36ef4a8d1b98b74f52963566ff
hmike54@gmail.com <hmike54@gmail.com>
2022-09-08 17:27:02 -0700

    main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
 
 int main(void)
 
