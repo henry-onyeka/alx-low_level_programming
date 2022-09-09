3ba71e907995cf0a05bdebec9c3c53fcd063719c
hmike54@gmail.com <hmike54@gmail.com>
2022-09-09 14:05:09 -0700

    printing lower and uppercase alphabets in same line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+* main - this betty dey mad i swear, just dey suffeer man pikin anyhow
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	char henry = 'a';
+
+	char hamza = 'A'
+		while (henry < z && hamza < Z)
+{
+	putchar(henry, hamza);
+	henry++
+	hamza++
+	putchar("\n"10);
+}
+return (0);
+}
