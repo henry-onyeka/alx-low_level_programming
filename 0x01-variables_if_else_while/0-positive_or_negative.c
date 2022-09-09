1c33d9149dbb4ed9a7c5175c6351c56bcc06819a
hmike54@gmail.com <hmike54@gmail.com>
2022-09-09 10:52:23 -0700

    first if conditional statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main -Determines if a number is positive, negative or zero
  *
- * Rturn: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
@@ -27,5 +27,5 @@ else if (n == 0)
 
 	printf("%d is zero\n", n);
 }
-return(0);
+return (0);
 }
