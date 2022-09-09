9f238dc9d3cbc31264eb148d315657da7a708c9f
hmike54@gmail.com <hmike54@gmail.com>
2022-09-09 12:13:27 -0700

    printing last digit in a random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
-*main - Determines either greater than 5, is less than 6, or is 0
-*Description: print last digit in random number
-*Return: always 0 (success)
-*/
+/**
+ *
+ *  * main - Determines either greater than 5, is less than 6, or is 0
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 int main(void)
 {
 	int final;
