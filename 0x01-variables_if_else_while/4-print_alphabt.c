9abe345398ae24f641ecec7ace9d6b0847c1cf83
hmike54@gmail.com <hmike54@gmail.com>
2022-09-09 15:49:53 -0700

    minus letter e and letter q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,14 @@ int main(void)
 {
 	char mumu = 'a';
 
-	while (mumu <= 'z' && mumu  != 'e' && mumu != 'q')
+	while (mumu <= 'z')
 	{
+		if (mumu != 'e' && mumu != 'q')
+		{
 		putchar(mumu);
+		}
 		mumu++;
 }
+putchar('\n');
 return (0);
 }
