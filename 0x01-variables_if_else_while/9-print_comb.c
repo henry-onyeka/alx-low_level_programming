ad6897ab0dc451d1d0f511a4beb95245e673537c
henry-onyeka <hmike54@gmail.com>
2022-09-10 03:09:58 +0100

    posiblle combinations of tabs and comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,22 +4,22 @@
  * Return: Always 0 (success)
  */
 int main(void)
- {
-	int i;
+{
+int i;
 
-	i = 48;
+i = 48;
 
-	while (i < 58)
-	{
-		putchar(i);
-		if (i != 57)
-		{
-			putchar(44);
-			putchar(32);
-		}
-		i++;
-	}
+while (i < 58)
+{
 
-	putchar(10);
-	return (0);
+putchar(i);
+if (i != 57)
+{
+putchar(44);
+putchar(32);
+}
+i++;
+}
+putchar(10);
+return (0);
 }
