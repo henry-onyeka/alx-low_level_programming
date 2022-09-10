7e5bc21da275de477950457c8aca029ec1d6c102
henry-onyeka <hmike54@gmail.com>
2022-09-10 02:27:27 +0100

    posiblle combinations of tabs and comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 /**
  * main - print print single digit numbers
- * Return : Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int num;
-	for (num = 0 ; num <= 9; num++)
+	int num = 0 ;
+
+	while (num <= 9)
 	{
 		putchar(num);
 		putchar(',');
 		putchar(' ');
+		num++;
 	}
 	putchar(10);
 	return (0);
