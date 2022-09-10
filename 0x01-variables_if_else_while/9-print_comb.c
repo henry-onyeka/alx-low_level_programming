f1c973feee96955c9de51f866548e8815aaa3ba5
henry-onyeka <hmike54@gmail.com>
2022-09-10 02:15:06 +0100

    posiblle combinations of tabs and comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-	int num = 0;
-	for (num <= 9; num++)
+	int num;
+	for (num = 0 ; num <= 9; num++)
 	{
 		putchar(num);
-		putchar(",\t")
+		putchar(',\t');
 	}
 	putchar(10);
 	return (0);
-
+}
