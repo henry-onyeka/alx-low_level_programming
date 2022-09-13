532d677887c0bacdb3fdf2ff0842fb4ba972d044
hmike54@gmail.com <hmike54@gmail.com>
2022-09-13 14:21:46 -0700

    print alphabet A-Z 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void 2-print_alphabet_x10.c(void)
+{
+	char alpha = 'a';
+	for (int i = 0; i <= 10; i++)
+	{
+		while (alpha <= 'z')
+{
+	_putchar(alpha);
+	alpha++;
+}
+}
+_putchar('\n');
+}
