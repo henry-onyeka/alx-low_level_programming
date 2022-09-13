40634e8fa81e0156feb243fe4ef6061b78f38b18
hmike54@gmail.com <hmike54@gmail.com>
2022-09-13 10:33:30 -0700

    calling a function that returns an empty function to print alphabelts

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+ *print_aphabet - empty return
+ *Return: empty from the void function
+ */
+int main()
+{
+void print_alphabet(void)
+{
+	char alpha = 'a' ;
+
+	while (alpha <= 'z')
+{
+	putchar(alpha);
+
+	alpha++;
+}
+putchar('\n');
+}
+return 0;
+}
