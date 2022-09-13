d59b580c9399a7a74684706d21a136b0b77e1d4e
hmike54@gmail.com <hmike54@gmail.com>
2022-09-13 16:03:26 -0700

    indicating lower case letters using return value

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,20 @@
 #include "main.h"
-
+/**
+ * _islower - just an if statement
+ * @c: is an Ascii character
+ * Return: always 0 bros
+*/
 int _islower(int c)
+
 {
 	if (c >= 'a' && c <= 'z')
-	{
+
+{
 		return (1);
-	}
-	else {
+
+}
+	else
+{
 		return (0);
-	}
+}
 }
