b905f98f3303d147c797756effd5670bea7a9a54
hmike54@gmail.com <hmike54@gmail.com>
2022-09-13 18:22:45 -0700

    printing last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+3include "main.h"
+/**
+ * print_last - is a gotten from the modulu of the variable
+ * @last: is an integer value
+ * Return: is always 0
+ */
+int print_last_digit(int last)
+{
+	int va = last % 10;
+
+_putchar("%d%", va);
+
+}
