b432944309ddd77fdef317569c7d5076fd0fea7e
hmike54@gmail.com <hmike54@gmail.com>
2022-09-13 17:13:27 -0700

    printiung positive, negative and zero signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - just to print sign in accordance with the output
+ * @n: is an integer
+ *Return: always nonsense
+ */
+
+int print_sign(int n)
+
+{
+	if (n < 0)
+{
+	_putchar('-');
+
+	return (-1);
+}
+else if (n > 0)
+{
+	_putchar('+');
+	return (1);
+}
+else if (n == 0)
+
+{ _putchar('0');
+	return (0);
+}
+}
