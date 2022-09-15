f2a170dc88d406aba69ffbecdc76dfa0b0b02013
hmike54@gmail.com <hmike54@gmail.com>
2022-09-15 11:22:41 -0700

    debugging proccess started

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: return 0 forn me
+ */
+int main(void)
+{
+	int i;
+
+	positive_or_negative(i);
+	return (0);
+
+}
