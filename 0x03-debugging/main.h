45491430f4d98e3573fd3c50ea4c83c6a2e696c7
hmike54@gmail.com <hmike54@gmail.com>
2022-09-15 12:49:43 -0700

    debug a function that prints larget numbers

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,9 +5,7 @@
 
 #include <stdio.h>
 
-int convert_day(month, day);
 void positive_or_negative(int i);
 int convert_day(int month, int day);
-int largest_number(a, b, c);
 int largest_number(int a, int b, int c);
 #endif /* MAIN_H */
