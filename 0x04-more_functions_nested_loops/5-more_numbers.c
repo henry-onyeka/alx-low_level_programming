60a8925713c72d6200711e5b7261ffae977710f2
hmike54@gmail.com <hmike54@gmail.com>
2022-09-15 17:30:04 -0700

    tirelessly pushing

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,26 @@
 #include "main.h"
+
 /**
  * more_numbers - prints more
  *
  * Return: empty
  */
-void more_numbers(void)
-{
-	int line, n, repeat, limit;
 
-	n = '0';
-	repeat = 0;
-	limit = '9';
+void more_numbers(void)
+
+{
+	int n = '0';
+int line;
+	int repeat = 0;
+	int limit = '9';
 
 	for (line = 0; line < 10; line++)
 	{
 		while (repeat < 2)
+
 		{
 			while (n <= limit)
+
 			{
 				if (limit == '4')
 					_putchar('1');
@@ -28,6 +32,7 @@ void more_numbers(void)
 			n = '0';
 		}
 		_putchar('\n');
+		repeat = 0;
 		n = '0';
 		limit = '9';
 	}
