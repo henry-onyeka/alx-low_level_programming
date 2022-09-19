307274201e9fcc9a996e2bb3225db8e334a44ff1
hmike54@gmail.com <hmike54@gmail.com>
2022-09-19 16:44:01 -0700

    reverse print

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,12 @@ void puts2(char *str)
 	i = 0;
 
 	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
 {
-if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
-	}
+		_putchar(str[i]);
+			i++;
+}
+}
 	_putchar('\n');
 }
