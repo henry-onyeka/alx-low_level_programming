c3f06890b391737f1bbb7a2bafd885a34606ae3b
hmike54@gmail.com <hmike54@gmail.com>
2022-09-19 13:59:09 -0700

    swapping a simple variable to another variable

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - swapping two given integers
+ * @a: first variable int
+ * @b: second variablwe
+ * Return: it returned a and b
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = *a;
+}
