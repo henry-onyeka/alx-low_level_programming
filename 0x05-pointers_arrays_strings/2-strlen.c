c42bd07fa202f05ce6f24d14967a0d7f1a7a8743
hmike54@gmail.com <hmike54@gmail.com>
2022-09-19 15:04:37 -0700

    counting strings now

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - this functions iterates and counts the strings in the array
+ * @s: thats the string arrays
+ * Return: returns thelenght of the array
+ */
+int _strlen(char *s)
+{
+	int p;
+
+	char s[] = "my alx peer group are  fantastic one";
+
+	for (p = 0; s[p] != '\0'; ++p)
+	{
+	}
+
+	return (p);
+}
+
