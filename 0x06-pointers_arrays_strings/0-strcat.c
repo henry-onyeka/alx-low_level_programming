b422de0ccfd865379fbeffc746912a132ab207ad
hmike54@gmail.com <hmike54@gmail.com>
2022-09-21 15:21:53 -0700

    nothing special. concatenating the two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,2 +1,27 @@
 #include "main.c"
+/**
+ * _strcat - shows how to add two strings
+ * @dest: is a char variable
+ * @src: is a variabble
+ * Return: wil be void
+ */
+void _strcat(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	char dest, src;
+
+	while (dest[a] != '\0')
+		a++;
+
+	while (src[b] != '\0')
+
+{
+	dest[a] += *src[b];
+	b++;
+	a++;
+}
+dest[a] = '\0';
+}
 
