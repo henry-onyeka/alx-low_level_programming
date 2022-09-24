08de53bc37e5c177254b0421aedf5f6a3fe01b4a
hmike54@gmail.com <hmike54@gmail.com>
2022-09-23 21:06:43 -0700

    now copying

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int c = 0;
 
+	while (c < n)
+	{
+
 	while (c < n && src[c] != '\0')
 	{
 		dest[c] = src[c];
 		c++;
 	}
-	while (c < n)
 	dest[c] = '\0';
 	c++;
+}
 	return (dest);
 }
