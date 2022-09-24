68582b3dd18f7c7a5c6df78249e8a14ba65ef3a2
hmike54@gmail.com <hmike54@gmail.com>
2022-09-23 20:50:47 -0700

    now copying

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 /**
  *  _strncpy - Entry point
@@ -16,32 +15,7 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[c] = src[c];
 		c++;
 	}
-	while (c < n)
-
 	dest[c] = '\0';
-	c++;
-	return (dest);
-}
-=======
-#include "main.h"
-
-/**
- * _strncpy - Entry point
- * @dest: copy to
- * @src: copy from
- * @n: input number of char
- * Return: Always 0 (Success)
- */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-		if (i < n)
-			dest[i] = src[i];
-		while (i < n)
-			dest[i++] = '\0';
 
 	return (dest);
 }
->>>>>>> 622025babd1a8100889e62b9fabdc874cb2b397e
