4d222632ceaa1ad0fc159375fe642f70e828a3af
hmike54@gmail.com <hmike54@gmail.com>
2022-09-26 15:38:30 -0700

    copying the memory location

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - cpo[pies a memory locatkion to anotjer file
+ * @dest: this is the destination to copy to
+ * @src: where to copy from
+ * @n: number of timesto copy
+ * Return: a pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
