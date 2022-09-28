22ba8de89f92250396040c69673ef74b73bb86a4
hmike54@gmail.com <hmike54@gmail.com>
2022-09-28 14:59:40 -0700

    recursively printing characters

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  _puts_recursion - prints the string s with recursion joor
+ *  @s: this the variable pointing to the string
+ *  Return: returns nothing based on the task
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+		_putchar('\n');
+	}
+}
