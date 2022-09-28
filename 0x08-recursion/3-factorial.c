282fa81f7965e57b594b52c21bee27627597c054
hmike54@gmail.com <hmike54@gmail.com>
2022-09-28 16:30:40 -0700

    recursively write a function that prints out the factorial of any given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - the function prints out thee factiorial of given number
+ * @n: is the parameter to be passed
+ * Return: -1 to throw an error
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		_putchar(n * (factorial(n - 1));
+}
+}
