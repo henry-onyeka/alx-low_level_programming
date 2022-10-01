723e36f8656b2c1f9919c80fddc2ea2fae408689
hmike54@gmail.com <hmike54@gmail.com>
2022-10-01 13:14:57 -0700

    multiply from the input gotten from the command line

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main function
+ * @mult: strings collector
+ * @argc: the number of arguments
+ * Return: nothin
+ */
+int main(int argc, char *mult[])
+{
+	int a = atoi(mult[1]);
+	int b = atoi(mult[2]);
+	int ans = a * b;
+
+	if (argc <= 2 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	
+	}
+	else
+	{
+	printf("%d multiplied by %d is = %d\n", a, b, ans);
+	}
+	return (0);
+}
