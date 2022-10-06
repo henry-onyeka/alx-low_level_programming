a86c6f7d8f27dd2d947d07af88bfd05770277274
hmike54@gmail.com <hmike54@gmail.com>
2022-10-06 15:42:32 -0700

    concatenate two

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,4 +10,43 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int pack, d = 0, a, b, c = 0;
+	char *lag;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (a = 0; s1[a] != '\0'; a++)
+{
+}
+
+for (b = 0; s2[b] != '\0'; b++)
+{
+}
+	pack = (s1[a]) + (s2[b] + 1);
+
+	if (n < b)
+		b = n;
+	b = a + b;
+
+	lag = malloc(pack * sizeof(char *));
+
+	if (lag == NULL)
+		return (NULL);
+	while (c < a)
+{
+		lag[c] = s1[c];
+		c++;
+}
+	while (d < b)
+{
+		lag[c] = s2[d];
+		d++;
+		c++;
+}
+	c++;
+	lag[c] = '\0';
+	return (lag);
+}
