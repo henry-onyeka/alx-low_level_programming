79123a5d0aba1809c01e4dd9ebbf06171bfceed6
MICHAEL HENRY ONYEKA <hmike54@gmail.com>
2022-10-06 16:05:52 -0700

    Update 1-string_nconcat.c
    its a wrap

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,50 +1,49 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 /**
- *  *string_nconcat - the function concatenates two string
- *  @n: is the numbee of integers
- *  @s1: first
- *  @s2: second one
- *  Return: a pointer
+ * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
+ * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
+ * @s1: input one
+ * @s2: input two
+ * @n: s2's number of bytes
+ * Return: 0
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int pack, d, a, b, c;
-	char *lag;
+	char *arr;
+	unsigned int i, j, co, co_2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-for (a = 0; s1[a] != '\0'; a++)
-{
-}
 
-for (b = 0; s2[b] != '\0'; b++)
-{
-}
-	pack = a + (b + 1);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
 
-	if (n < b)
-		b = n;
-	b = a + b;
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
 
-	lag = malloc(pack * sizeof(char *));
+	if (n < j)
+		j = n;
 
-	if (lag == NULL)
+	j += i;
+	arr = malloc(sizeof(char *) * (j + 1));
+
+	if (arr == NULL)
 		return (NULL);
-	for (c = 0; c < a; c++)
-{
-		lag[c] = s1[c];
-		
-}
-	for (d = 0; d < b; d++)
-{
-		lag[c] = s2[d];
-		c++;
-}
-	c++;
-	lag[c] = '\0';
-	return (lag);
+
+	for (co = 0; co < i; co++)
+		arr[co] = s1[co];
+	for (co_2 = 0; co < j; co_2++)
+	{
+		arr[co] = s2[co_2];
+		co++;
+	}
+	co++;
+	arr[co] = '\0';
+	return (arr);
 }
