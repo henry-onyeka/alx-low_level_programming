ad01ec0de1a9a70d3e91bc521515c33baae9d603
hmike54@gmail.com <hmike54@gmail.com>
2022-10-06 14:15:17 -0700

    allocate this mem size

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *  *string_nconcat - the function concatenates two string
+ *  @n: is the numbee of integers
+ *  @s1: first
+ *  @s2: second one
+ *  Return: a pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
