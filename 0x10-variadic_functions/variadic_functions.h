0761c25e3ab651445574f268b7ed7d1241293552
MICHAEL HENRY ONYEKA <hmike54@gmail.com>
2022-10-13 11:35:25 -0700

    Update variadic_functions.h
    kol

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,11 @@
-#ifndef _VARIADIC_FUNTIONS_H_
-#define _VARIADIC_FUNCTIONS_H_
+#ifndef _VARIADIC_FUNTIONS_H
+#define _VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
+#include <stdio.h>
+
 
 void print_all(const char * const format, ...);
-char _putchar(char c);
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
