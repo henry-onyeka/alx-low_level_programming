7f6a19c117fe773f749014fa9ae548d4b7c582ed
MICHAEL HENRY ONYEKA <hmike54@gmail.com>
2022-10-13 11:30:51 -0700

    Update variadic_functions.h
    re-do

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,23 @@
 #ifndef _VARIADIC_FUNTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 
+
+
+typedef struct pter
+{
+	char *test;
+	void (*printer)();
+} pt;
+
+void print_int(va_list arg);
+
+void print_char(va_list arg);
+
+void print_float(va_list arg);
+
+void print_string(va_list arg);
+
+void print_all(const char * const format, ...);
 char _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
