5f4e450c4273334c05913347f1e66ca266ed42ed
hmike54@gmail.com <hmike54@gmail.com>
2022-10-14 18:20:32 -0700

    p

---
+++ 0x0F-function_pointers/printmy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+void printn(char *name, void (*f)(char *))
+{
+
+if (name != NULL || f != NULL)
+f(name);
+}
+int main()
+{
+printn(henry);
+printf("%s",f(name));
+}
+
+
