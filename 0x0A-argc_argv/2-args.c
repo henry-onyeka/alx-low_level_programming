3f959ab7b852dc72cacaf3228fd4414c13d316d7
henry-onyeka <hmike54@gmail.com>
2022-10-25 16:56:35 +0000

    updating

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,13 +5,13 @@
  * @thestring: is the characters
  * Return: nothing in return
  */
-int main(int thenum, char *thestring[])
+int main(int thenum, char *chop[])
 {
 	int t = 0;
 
 	for (t = 0; t < thenum; t++)
 	{
-		printf("%s\n", thestring[t]);
+		printf("%s\n", chop[t]);
 	}
 	return (0);
 }
