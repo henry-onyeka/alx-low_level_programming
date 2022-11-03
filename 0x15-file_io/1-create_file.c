36234644e0fc313f1263d3ed3163c8a357e3c8c9
henry-onyeka <hmike54@gmail.com>
2022-11-03 00:35:31 +0000

    updating

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,34 +9,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, wr, rd;
-int num;
+int fd;
+int nletters;
+int rwr;
 
-fd = open(filename, O_CREATE | O_RDONLY |  O_TRUNC, 0600);
-
+if (!filename)
+return (-1);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
 if (!text_content)
 text_content = "";
+for (nletters = 0; text_content[nletters]; nletters++)
+{
+rwr = write(fd, text_content, nletters);
 
-if (filename == NULL && fd == -1)
-{
+}
+if (rwr == -1)
 return (-1);
-}
-else
-{
-return (1);
-}
-for (num = 0; text_content[num]; num++)
-{
-wr = write(fd, text_content, num);
-}
-if (wr == -1)
-{
-return (-1);
-}
-else
-{
-return (1);
-}
 close(fd);
 return (1);
 }
